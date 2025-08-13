4c4784ee134a7b4ecfa7ca8f295351657b0b0735
satyajit <satyajit_jbl@yahoo.com>
2025-08-13 22:20:40 +0600

    first commit with fist function

---
+++ 3. function.py
@@ -0,0 +1,19 @@
+#   Math book no: 86 ("Aljebra")
+a =int(input("Enter a number for a"))
+b =int(input("Enter a number for b"))
+c = int(input("Enter a number for c"))
+def solution():
+    # d =(a^2)+(b^2)+(c^2)
+    d = a+b+c
+    print(d)
+    
+solution()
+ 
+
+# x ="soumik"
+
+# def myfunc():
+#     global x
+#     x= "Awesome"
+# myfunc()
+# print("Python is "+x)    
