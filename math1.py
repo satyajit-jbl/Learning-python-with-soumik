1741f3d5cddba6efdd07f103cca26e9aab7a3447
satyajit <satyajit_jbl@yahoo.com>
2025-08-16 16:12:40 +0600

    math1 edited

--- math1.py
+++ math1.py
@@ -1,6 +1,6 @@
-a =int(input("Enter a number a"))
-b =int(input('Enter a number b'))
-c =int(input('Enter a number c'))
+a =int(input("Enter a number a : "))
+b =int(input('Enter a number b : '))
+c =int(input('Enter a number c : '))
 def myfunc():
    d =(a**2+2*a*b-c)
    print(d)
