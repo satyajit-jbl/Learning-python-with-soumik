76fd3229b95aa1a9c4353e7ac2c16e97ca0126d1
satyajit <satyajit_jbl@yahoo.com>
2025-09-15 09:28:36 +0600

    rEVISION

--- math1.py
+++ math1.py
@@ -1,7 +1,16 @@
-a =int(input("Enter a number a : "))
-b =int(input('Enter a number b : '))
-c =int(input('Enter a number c : '))
-def myfunc():
-   d =(a**2+2*a*b-c)
-   print(d)
-myfunc()   
+# a =int(input("Enter a number a : "))
+# b =int(input('Enter a number b : '))
+# c =int(input('Enter a number c : '))
+# def myfunc():
+#    d =(a**2+2*a*b-c)
+#    print(d)
+# myfunc()   
+# print 
+
+
+num1 =[1,2,3]
+num2 = (4,5,6)
+# Addition = Letter + Alphata
+# print(Addition)
+num1.extend(num2)
+print(num1)
